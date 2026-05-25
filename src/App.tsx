d5277848b1e026a1d6d08393a9bdabc2f413cc5f
kouta-wolf <koutaookami18@gmail.com>
2026-05-25 17:19:26 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App () {
+  return (
+    <div className="bg-slate-50 min-h-screen flex items-center justify-center font-bold text-4xl">
+      <p>Hello,React + TailWindcss</p>
+    </div>
+  );
+}
