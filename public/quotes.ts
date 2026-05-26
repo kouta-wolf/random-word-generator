ae5da7bf86bcc6783eadcc546217d829ccb61e64
kouta-wolf <koutaookami18@gmail.com>
2026-05-26 09:38:48 +0900

    chore: 疑似API用のデータを取り込み

---
+++ public/quotes.ts
@@ -0,0 +1,622 @@
+// src/data/quotes.ts
+export const quotes = [
+  // Linus Torvalds
+  {
+    id: 1,
+    author: "Linus Torvalds",
+    quote:
+      "I'm doing a free operating system (just a hobby, won't be big and professional like GNU).",
+  },
+  {
+    id: 2,
+    author: "Linus Torvalds",
+    quote:
+      "See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too ;-)",
+  },
+  {
+    id: 3,
+    author: "Linus Torvalds",
+    quote: "Talk is cheap. Show me the code.",
+  },
+  {
+    id: 4,
+    author: "Linus Torvalds",
+    quote:
+      "If you need more than 3 levels of indentation, you're screwed anyway, and should fix your program.",
+  },
+  {
+    id: 5,
+    author: "Linus Torvalds",
+    quote: "If you think your users are idiots, only idiots will use it.",
+  },
+  {
+    id: 6,
+    author: "Linus Torvalds",
+    quote:
+      "I will, in fact, claim that the difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
+  },
+  {
+    id: 7,
+    author: "Linus Torvalds",
+    quote:
+      "Really, I'm not out to destroy Microsoft. That will just be a completely unintentional side effect.",
+  },
+  {
+    id: 8,
+    author: "Linus Torvalds",
+    quote: "If Microsoft ever does applications for Linux it means I've won.",
+  },
+  {
+    id: 9,
+    author: "Linus Torvalds",
+    quote:
+      "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
+  },
+  {
+    id: 10,
+    author: "Linus Torvalds",
+    quote:
+      "An infinite number of monkeys typing into GNU Emacs would never make a good program.",
+  },
+
+  // Paul Graham
+  {
+    id: 11,
+    author: "Paul Graham",
+    quote:
+      "It's not so important what you work on, so long as you're not wasting your time.",
+  },
+  {
+    id: 12,
+    author: "Paul Graham",
+    quote:
+      "There are few sources of energy so powerful as a procrastinating grad student.",
+  },
+  {
+    id: 13,
+    author: "Paul Graham",
+    quote: "The top 5% of programmers probably write 99% of the good software.",
+  },
+  {
+    id: 14,
+    author: "Paul Graham",
+    quote:
+      "The most important thing is to be able to think what you want, not to say what you want.",
+  },
+  {
+    id: 15,
+    author: "Paul Graham",
+    quote:
+      "The best thing software can be is easy, but the way to do this is to get the defaults right, not to limit users' choices.",
+  },
+  {
+    id: 16,
+    author: "Paul Graham",
+    quote:
+      "In programming the hard part isn't solving problems, but deciding what problems to solve.",
+  },
+  {
+    id: 17,
+    author: "Paul Graham",
+    quote: "The most valuable insights are both general and surprising.",
+  },
+  {
+    id: 18,
+    author: "Paul Graham",
+    quote: "Live in the future, then build what's missing.",
+  },
+
+  // Steve Jobs
+  {
+    id: 19,
+    author: "Steve Jobs",
+    quote: "Innovation distinguishes between a leader and a follower.",
+  },
+  {
+    id: 20,
+    author: "Steve Jobs",
+    quote:
+      "Design is not just what it looks like and feels like. Design is how it works.",
+  },
+  {
+    id: 21,
+    author: "Steve Jobs",
+    quote: "Stay hungry, stay foolish.",
+  },
+  {
+    id: 22,
+    author: "Steve Jobs",
+    quote:
+      "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.",
+  },
+  {
+    id: 23,
+    author: "Steve Jobs",
+    quote:
+      "It's really hard to design products by focus groups. A lot of times, people don't know what they want until you show it to them.",
+  },
+  {
+    id: 24,
+    author: "Steve Jobs",
+    quote:
+      "Quality is more important than quantity. One home run is much better than two doubles.",
+  },
+  {
+    id: 25,
+    author: "Steve Jobs",
+    quote:
+      "I'm convinced that about half of what separates the successful entrepreneurs from the non-successful ones is pure perseverance.",
+  },
+  {
+    id: 26,
+    author: "Steve Jobs",
+    quote:
+      "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations.",
+  },
+
+  // Elon Musk
+  {
+    id: 27,
+    author: "Elon Musk",
+    quote:
+      "When something is important enough, you do it even if the odds are not in your favor.",
+  },
+  {
+    id: 28,
+    author: "Elon Musk",
+    quote:
+      "I think it's very important to have a feedback loop, where you're constantly thinking about what you've done and how you could be doing it better.",
+  },
+  {
+    id: 29,
+    author: "Elon Musk",
+    quote:
+      "Failure is an option here. If things are not failing, you are not innovating enough.",
+  },
+  {
+    id: 30,
+    author: "Elon Musk",
+    quote:
+      "The path to the CEO's office should not be through the CFO's office, and it should not be through the marketing department. It needs to be through engineering and design.",
+  },
+  {
+    id: 31,
+    author: "Elon Musk",
+    quote:
+      "People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy working.",
+  },
+  {
+    id: 32,
+    author: "Elon Musk",
+    quote:
+      "I think it's important to reason from first principles rather than by analogy.",
+  },
+  {
+    id: 33,
+    author: "Elon Musk",
+    quote:
+      "If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, it's not.",
+  },
+  {
+    id: 34,
+    author: "Elon Musk",
+    quote:
+      "The first step is to establish that something is possible; then probability will occur.",
+  },
+
+  // Mark Zuckerberg
+  {
+    id: 35,
+    author: "Mark Zuckerberg",
+    quote:
+      "The biggest risk is not taking any risk. In a world that's changing really quickly, the only strategy that is guaranteed to fail is not taking risks.",
+  },
+  {
+    id: 36,
+    author: "Mark Zuckerberg",
+    quote:
+      "Move fast and break things. Unless you are breaking stuff, you are not moving fast enough.",
+  },
+  {
+    id: 37,
+    author: "Mark Zuckerberg",
+    quote:
+      "I think a simple rule of business is, if you do the things that are easier first, then you can actually make a lot of progress.",
+  },
+  {
+    id: 38,
+    author: "Mark Zuckerberg",
+    quote:
+      "In a world that's changing so quickly, the biggest risk you can take is not taking any risk.",
+  },
+  {
+    id: 39,
+    author: "Mark Zuckerberg",
+    quote:
+      "The question isn't, 'What do we want to know about people?', It's, 'What do people want to tell about themselves?'",
+  },
+  {
+    id: 40,
+    author: "Mark Zuckerberg",
+    quote: "Building a mission and building a business go hand in hand.",
+  },
+
+  // Reid Hoffman
+  {
+    id: 41,
+    author: "Reid Hoffman",
+    quote:
+      "If you are not embarrassed by the first version of your product, you've launched too late.",
+  },
+  {
+    id: 42,
+    author: "Reid Hoffman",
+    quote:
+      "The entrepreneur always searches for change, responds to it, and exploits it as an opportunity.",
+  },
+  {
+    id: 43,
+    author: "Reid Hoffman",
+    quote:
+      "All humans are entrepreneurs not because they should start companies but because the will to create is encoded in human DNA.",
+  },
+  {
+    id: 44,
+    author: "Reid Hoffman",
+    quote:
+      "An entrepreneur is someone who jumps off a cliff and builds a plane on the way down.",
+  },
+  {
+    id: 45,
+    author: "Reid Hoffman",
+    quote:
+      "The person passionate about what he or she is doing will outwork and outlast the guy motivated solely by making money.",
+  },
+  {
+    id: 46,
+    author: "Reid Hoffman",
+    quote:
+      "No matter how brilliant your mind or strategy, if you're playing a solo game, you'll always lose out to a team.",
+  },
+
+  // Jeff Bezos
+  {
+    id: 47,
+    author: "Jeff Bezos",
+    quote:
+      "Your brand is what other people say about you when you're not in the room.",
+  },
+  {
+    id: 48,
+    author: "Jeff Bezos",
+    quote:
+      "If you're not stubborn, you'll give up on experiments too soon. And if you're not flexible, you'll pound your head against the wall and you won't see a different solution to a problem you're trying to solve.",
+  },
+  {
+    id: 49,
+    author: "Jeff Bezos",
+    quote:
+      "In the old world, you devoted 30% of your time to building a great service and 70% of your time to shouting about it. In the new world, that inverts.",
+  },
+  {
+    id: 50,
+    author: "Jeff Bezos",
+    quote:
+      "If you double the number of experiments you do per year you're going to double your inventiveness.",
+  },
+  {
+    id: 51,
+    author: "Jeff Bezos",
+    quote:
+      "We've had three big ideas at Amazon that we've stuck with for 18 years, and they're the reason we're successful: Put the customer first. Invent. And be patient.",
+  },
+  {
+    id: 52,
+    author: "Jeff Bezos",
+    quote:
+      "A brand for a company is like a reputation for a person. You earn reputation by trying to do hard things well.",
+  },
+
+  // Sheryl Sandberg
+  {
+    id: 53,
+    author: "Sheryl Sandberg",
+    quote: "Done is better than perfect.",
+  },
+  {
+    id: 54,
+    author: "Sheryl Sandberg",
+    quote:
+      "If you're offered a seat on a rocket ship, don't ask what seat! Just get on.",
+  },
+  {
+    id: 55,
+    author: "Sheryl Sandberg",
+    quote:
+      "Leadership is about making others better as a result of your presence and making sure that impact lasts in your absence.",
+  },
+  {
+    id: 56,
+    author: "Sheryl Sandberg",
+    quote:
+      "It's not about having it all. It's about having what you value most.",
+  },
+
+  // Bill Gates
+  {
+    id: 57,
+    author: "Bill Gates",
+    quote: "Your most unhappy customers are your greatest source of learning.",
+  },
+  {
+    id: 58,
+    author: "Bill Gates",
+    quote:
+      "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+  },
+  {
+    id: 59,
+    author: "Bill Gates",
+    quote:
+      "We always overestimate the change that will occur in the next two years and underestimate the change that will occur in the next ten.",
+  },
+  {
+    id: 60,
+    author: "Bill Gates",
+    quote:
+      "It's fine to celebrate success but it is more important to heed the lessons of failure.",
+  },
+  {
+    id: 61,
+    author: "Bill Gates",
+    quote:
+      "As we look ahead into the next century, leaders will be those who empower others.",
+  },
+
+  // Larry Page
+  {
+    id: 62,
+    author: "Larry Page",
+    quote:
+      "If you're changing the world, you're working on important things. You're excited to get up in the morning.",
+  },
+  {
+    id: 63,
+    author: "Larry Page",
+    quote: "Always deliver more than expected.",
+  },
+  {
+    id: 64,
+    author: "Larry Page",
+    quote:
+      "Lots of companies don't succeed over time. What do they fundamentally do wrong? They usually miss the future.",
+  },
+  {
+    id: 65,
+    author: "Larry Page",
+    quote: "It's very hard to fail completely if you aim high enough.",
+  },
+
+  // Satya Nadella
+  {
+    id: 66,
+    author: "Satya Nadella",
+    quote:
+      "Our industry does not respect tradition. It only respects innovation.",
+  },
+  {
+    id: 67,
+    author: "Satya Nadella",
+    quote:
+      "The view you adopt for yourself profoundly affects the way you lead your life.",
+  },
+  {
+    id: 68,
+    author: "Satya Nadella",
+    quote:
+      "We needed to move from a know-it-all culture to a learn-it-all culture.",
+  },
+  {
+    id: 69,
+    author: "Satya Nadella",
+    quote: "The C in CEO stands for culture.",
+  },
+
+  // Jack Ma
+  {
+    id: 70,
+    author: "Jack Ma",
+    quote:
+      "If you don't give up, you still have a chance. Giving up is the greatest failure.",
+  },
+  {
+    id: 71,
+    author: "Jack Ma",
+    quote:
+      "Never give up. Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine.",
+  },
+  {
+    id: 72,
+    author: "Jack Ma",
+    quote:
+      "You should learn from your competitor, but never copy. Copy and you die.",
+  },
+  {
+    id: 73,
+    author: "Jack Ma",
+    quote:
+      "Today is cruel. Tomorrow is crueler. And the day after tomorrow is beautiful.",
+  },
+
+  // Susan Wojcicki
+  {
+    id: 74,
+    author: "Susan Wojcicki",
+    quote:
+      "Though we do need more women to graduate with technical degrees, I always like to remind women that you don't need to have science or technology degrees to build a career in tech.",
+  },
+  {
+    id: 75,
+    author: "Susan Wojcicki",
+    quote: "Work smart. Get things done. No nonsense. Move fast.",
+  },
+
+  // Sam Altman
+  {
+    id: 76,
+    author: "Sam Altman",
+    quote:
+      "The best CEOs I know are humble, and take in a lot of data. They have this connection between vision and execution.",
+  },
+  {
+    id: 77,
+    author: "Sam Altman",
+    quote:
+      "It's better to have 100 people love you than a million people like you.",
+  },
+  {
+    id: 78,
+    author: "Sam Altman",
+    quote:
+      "The hard part of standing on an exponential curve is: when you look backwards, it looks flat, and when you look forward, it looks vertical.",
+  },
+
+  // Sundar Pichai
+  {
+    id: 79,
+    author: "Sundar Pichai",
+    quote:
+      "A person who is happy is not because everything is right in his life, he is happy because his attitude towards everything in his life is right.",
+  },
+  {
+    id: 80,
+    author: "Sundar Pichai",
+    quote: "Wear your failures as a badge of honor.",
+  },
+  {
+    id: 81,
+    author: "Sundar Pichai",
+    quote:
+      "It's important to follow your dreams and heart. Do something that excites you.",
+  },
+
+  // Marc Andreessen
+  {
+    id: 82,
+    author: "Marc Andreessen",
+    quote: "Software is eating the world.",
+  },
+  {
+    id: 83,
+    author: "Marc Andreessen",
+    quote: "The only thing that matters is getting to product/market fit.",
+  },
+  {
+    id: 84,
+    author: "Marc Andreessen",
+    quote:
+      "Raise prices. If people keep buying, you've got a business. If not, you've got a hobby.",
+  },
+
+  // Guy Kawasaki
+  {
+    id: 85,
+    author: "Guy Kawasaki",
+    quote: "Ideas are easy. Implementation is hard.",
+  },
+  {
+    id: 86,
+    author: "Guy Kawasaki",
+    quote:
+      "The best reason to start an organization is to make meaning – to create a product or service that makes the world a better place.",
+  },
+
+  // Tim Cook
+  {
+    id: 87,
+    author: "Tim Cook",
+    quote: "Let your joy be in your journey - not in some distant goal.",
+  },
+  {
+    id: 88,
+    author: "Tim Cook",
+    quote:
+      "You want to be the pebble in the pond that creates the ripple for change.",
+  },
+
+  // Drew Houston
+  {
+    id: 89,
+    author: "Drew Houston",
+    quote: "Don't worry about failure; you only have to be right once.",
+  },
+  {
+    id: 90,
+    author: "Drew Houston",
+    quote:
+      "The hardest-working people don't work hard because they're disciplined. They work hard because working on an exciting problem is fun.",
+  },
+
+  // Brian Chesky
+  {
+    id: 91,
+    author: "Brian Chesky",
+    quote:
+      "Build something 100 people love, not something 1 million people kind of like.",
+  },
+  {
+    id: 92,
+    author: "Brian Chesky",
+    quote:
+      "If we tried to think of a good idea, we wouldn't have been able to think of a good idea. You just have to find the solution for a problem in your own life.",
+  },
+
+  // Marissa Mayer
+  {
+    id: 93,
+    author: "Marissa Mayer",
+    quote:
+      "I always did something I was a little not ready to do. I think that's how you grow.",
+  },
+  {
+    id: 94,
+    author: "Marissa Mayer",
+    quote:
+      "To me, innovations are the wheel, the car, electricity, the internet. An innovation is something that drastically alters the playing field.",
+  },
+
+  // Additional Quotes
+  {
+    id: 95,
+    author: "Aaron Levie",
+    quote:
+      "Customers don't care about your solution. They care about their problems.",
+  },
+  {
+    id: 96,
+    author: "Stewart Butterfield",
+    quote:
+      "Find the right balance between the features you want to build, the feasibility of them, and the time you have.",
+  },
+  {
+    id: 97,
+    author: "Ginni Rometty",
+    quote: "Growth and comfort do not coexist.",
+  },
+  {
+    id: 98,
+    author: "Richard Branson",
+    quote:
+      "Business opportunities are like buses, there's always another one coming.",
+  },
+  {
+    id: 99,
+    author: "Peter Thiel",
+    quote: "Competition is for losers. Be a monopoly.",
+  },
+  {
+    id: 100,
+    author: "Naval Ravikant",
+    quote: "Play long-term games with long-term people.",
+  },
+];
