67dc2f4b20a571eef1130d5ae6b8b9fb91566f5b
kouta-wolf <koutaookami18@gmail.com>
2026-05-26 09:32:59 +0900

    feat: ボタンを押す度に1~100のランダムな数字を出力

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { randomQuoteId } from "./Generator.ts";
+import { useState } from "react";
+
 export default function App() {
+  const [quoteId, setQuoteId] = useState(0);
+  const handleClick = () => {
+    const newId = randomQuoteId();
+    setQuoteId(newId);
+  }
   return (
+    <>
     <div className="bg-slate-50 min-h-screen flex flex-col items-center justify-center">
       <div>
         <h1 className="font-bold text-4xl text-slate-800 mb-8">
@@ -11,17 +20,24 @@ export default function App() {
           先人の知恵に触れてモチベーションを高めましょう！
         </p>
       </div>
-      <div className="px-8 py-4 bg-slate-800 text-white rounded-2xl mt-8 cursor-pointer hover:bg-slate-700">
+      <button
+        onClick={handleClick}
+        className="px-8 py-4 bg-slate-800 text-white rounded-2xl mt-8 cursor-pointer hover:bg-slate-700"
+      >
         Generate
-      </div>
+      </button>
+      <div id="result">結果(デバッグ): {quoteId}</div>
       <div className="bg-linear-90 from-blue-600 via-blue-700 to-blue-800 text-white rounded-2xl mt-8 py-24 px-8 flex flex-col items-center justify-center gap-16">
         <div className="quotes">
-          <p>名言を言ってるよ。名言を言ってるよ。名言を言ってるよ。名言を言ってるよ。</p>
+          <p>
+            名言を言ってるよ。名言を言ってるよ。名言を言ってるよ。名言を言ってるよ。
+          </p>
         </div>
         <div className="author">
           <p>作者名</p>
         </div>
       </div>
     </div>
+    </>
   );
 }
---
+++ src/Generator.ts
@@ -0,0 +1,3 @@
+export const randomQuoteId = () => {
+  return Math.floor(Math.random() * 100) + 1
+} ;
